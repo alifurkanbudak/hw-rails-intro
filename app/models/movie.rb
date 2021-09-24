e556be6bdb3083a8407af9bb2f1ce071f211f516
EC2 Default User <ec2-user@ip-172-31-29-191.us-east-2.compute.internal>
2021-09-24 02:30:30 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -7,6 +7,7 @@ class Movie < ActiveRecord::Base
         # return Movie.all
         
         # logger.debug("Movie.with_ratings #{ratings}")
-        Movie.where(rating: ratings)
+        Movie.where('"rating"' +
+                         " ILIKE ANY ( array[?] )", ratings.map {|r| "%#{r}%" })
     end
 end
